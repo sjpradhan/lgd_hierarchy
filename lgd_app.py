3f1e9b0311b58288378452c129083d715c04ec87
sjpradhan <104523422+sjpradhan@users.noreply.github.com>
2024-06-20 18:01:51 +0530

    update

--- lgd_app.py
+++ lgd_app.py
@@ -361,13 +361,14 @@ def main():
 
     try:
         top_dist = stats_table[['States', 'Districts']].sort_values(by='Districts', ascending=False).head(5)
-        # fig = go.Figure(data=[go.Pie(labels=top_dist['States'], values=top_dist['Districts'], hole=0.5)])
-        # fig.update_layout(title='Top 10 States most Districts')
-        # fig.update_traces(hole=0.6)
-        # with col1:
-        #     st.plotly_chart(fig)
+        # st.write(top_dist)
+        fig = go.Figure(data=[go.Pie(labels=top_dist['States'], values=top_dist['Districts'], hole=0.5)])
+        fig.update_layout(title='Top 10 States most Districts')
+        fig.update_traces(hole=0.6)
+        with col1:
+            st.plotly_chart(fig)
     except Exception as e:
-        st.error(f"an error occur,{e}")
+        st.error(f"An error occurred: {e}")
         pass
     #
     # top_sub_dist = stats_table[['States', 'Sub-Districts']].sort_values(by='Sub-Districts', ascending=False).head(5)
